f9b9f8c74e250be2c2543650516c2f53522ca8c8
Palash <ng.palash.das@gmail.com>
2024-03-30 23:06:24 +0530

    aborted project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,30 @@
 import "./globals.css";
+import { DM_Sans } from "next/font/google";
+
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
-import { DM_Sans } from 'next/font/google'
-
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const dmSans = DM_Sans({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-dm-sans',
-})
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-dm-sans",
+});
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en" className={dmSans.variable}>
-      <body>{children}</body>
-      <PrismicPreview repositoryName={repositoryName} />
+      <body className="grid bg-[#070815] text-white">
+        <Header />
+        <main>{children}</main>
+        <Footer />
+        <PrismicPreview repositoryName={repositoryName} />
+      </body>
     </html>
   );
 }
